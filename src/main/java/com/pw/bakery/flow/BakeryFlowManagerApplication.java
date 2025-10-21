865c42792b927179d9986fbb42da6402de3dcd7d
parit <parit@wealth.co.th>
2025-10-21 22:56:00 +0700

    Initial commit: Bakery Flow Manager Spring Boot project

---
+++ src/main/java/com/pw/bakery/flow/BakeryFlowManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pw.bakery.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryFlowManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakeryFlowManagerApplication.class, args);
+	}
+
+}
